67221ad735f489dc6f3ef77abaf2b1e58ca9f3a8
Darya <dasha-439@yandex.ru>
2025-04-07 14:45:43 +0300

    Изменены режимы доступа, добавлены сеттеры и геттеры

--- src/category.py
+++ src/category.py
@@ -1,5 +1,7 @@
 import typing
 
+from src.product import Product
+
 
 class Category:
     """Класс для представления категории продукта"""
@@ -15,6 +17,20 @@ class Category:
         """Метод для инициализации экземпляра класса. Задаем значения атрибутам экземпляра."""
         self.name = name
         self.description = description
-        self.products = products if products else []
+        self.__products = products if products else []
         Category.category_count += 1
         Category.product_count += len(products)
+
+
+    @property
+    def products(self):
+        product_str = ""
+        for product in self.__products:
+            product_str += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
+        return product_str
+
+
+    def add_product(self, product: Product):
+        self.__products.append(product)
+        Category.category_count += 1
+        Category.product_count += 1
--- src/product.py
+++ src/product.py
@@ -1,6 +1,5 @@
 import typing
 
-
 class Product:
     """Класс для представления продукта"""
 
@@ -14,5 +13,26 @@ class Product:
         """Метод для инициализации экземпляра класса. Задаем значения атрибутам экземпляра."""
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+
+    @classmethod
+    def new_product(cls, new_product: dict):
+        if list(new_product.keys()) == ["name", "description", "price", "quantity"]:
+            name, description, price, quantity = new_product.values()
+            return cls(name, description, price, quantity)
+        else:
+            print("Входные данные не корректны")
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price):
+        if float(new_price) == 0 or float(new_price) < 0:
+            print("Цена не должна быть нулевая или отрицательная")
+
+        self.__price = new_price
+
