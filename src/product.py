15e0b6bed4cac069270a59b0b49eef0a79e3235f
Darya <dasha-439@yandex.ru>
2025-04-20 15:44:30 +0300

    Создан миксин класс PrintMixin

--- src/product.py
+++ src/product.py
@@ -1,9 +1,10 @@
 import typing
 
 from src.base_product import BaseProduct
+from src.print_mixin import PrintMixin
 
 
-class Product(BaseProduct):
+class Product(BaseProduct, PrintMixin):
     """Класс для представления продукта"""
 
     name: str
@@ -20,6 +21,7 @@ class Product(BaseProduct):
         self.__price = price
         self.quantity = quantity
         Product.products_list.append(self)
+        super().__init__()
 
     def __str__(self):
         return f"{self.name}, {self.__price} руб. Остаток: {self.quantity} шт."
